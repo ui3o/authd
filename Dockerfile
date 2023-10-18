937592c8363f3eff9a3de28e6672f6a5543e331e
ui3o.com <ui3o.com@gmail.com>
2023-10-18 21:56:41 +0200

    gulp build simplified

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,11 @@ FROM docker.io/golang:1.21 as golang
 COPY . /opt/authd
 WORKDIR /opt/authd
 RUN go version && go get ./...
-RUN ls -al
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w' -o authd *.go
 
 FROM docker.io/node:20-alpine as node
 COPY . /opt/authd
 WORKDIR /opt/authd/html
-RUN rm -rf ../assets && ls -al ../
 RUN yarn && yarn run fix && yarn run build
 
 FROM alpine:latest
--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,12 +1,11 @@
-const { src, dest, task } = require('gulp');
+const { src, task } = require('gulp');
 const clean = require('gulp-clean');
 const inlineSource = require('gulp-inline-source');
-const rename = require('gulp-rename');
 
 task('clean', () => {
     return src('dist', { read: false, allowEmpty: true }).pipe(clean());
 });
 
 task('inline', () => {
-    return src('dist/index.html').pipe(inlineSource()).pipe(rename('inline.html')).pipe(dest('dist/'));
+    return src('dist/index.html').pipe(inlineSource());
 });
